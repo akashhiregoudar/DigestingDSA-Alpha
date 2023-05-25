497ca9bddb613f32b19393129c68c22bc7bca9b8
Akashgouda Hiregoudar <akashhiregoudar1@gmail.com>
2023-05-25 19:57:46 +0530

    adding optimized code for finding x power n

--- Recursion/CalcXpowN.java
+++ Recursion/CalcXpowN.java
@@ -12,6 +12,19 @@ public class CalcXpowN {
         return (2 * findxpn(x, n / 2) * findxpn(x, n / 2));
     }
 
+    // More optimized than obove function as it calls recursivilly more times
+
+    public static int moreOptimizedFindxpn(int x, int n) {
+        if (n == 1) {
+            return x;
+        }
+        int halfPower = findxpn(x, n / 2);
+        if ((n & 1) == 0) {
+            return halfPower * halfPower;
+        }
+        return 2 * halfPower * halfPower;
+    }
+
     public static void main(String[] args) {
         int x = 2;
         int n = 10;
